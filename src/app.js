b6e3854da31ee6ea6ce92745deb5bd92513a18a2
AmandaJ17 <amanda.stacey17@gmail.com>
2020-10-28 16:47:15 +1300

    Added feels like element
    note - not connecting yet

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityName = document.querySelector("#city");
   let description = document.querySelector("#weatherDescription");
+  let feelsLike = document.querySelector("#feelsLike");
+
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
   cityName.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
+  feelsLike.innerHMTL = `Feels like ${Math.round(
+    response.data.main.feels_like
+  )}°C`;
 }
 let apiKey = "653b806439282a7bd7eba7fc4fe797a3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=auckland&units=metric&appid=${apiKey}`;
