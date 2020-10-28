1e196a316d72a2f15a7c42eb287a4a3e25b0af24
AmandaJ17 <amanda.stacey17@gmail.com>
2020-10-28 17:08:32 +1300

    Added max/min temps

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ function displayTemperature(response) {
   let cityName = document.querySelector("#city");
   let description = document.querySelector("#weatherDescription");
   let feelsLike = document.querySelector("#feelsLike");
+  let temperatureRange = document.querySelector("#temperatureRange");
 
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
   cityName.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
-  feelsLike.innerHMTL = `Feels like ${Math.round(
+  feelsLike.innerHTML = `Feels like ${Math.round(
     response.data.main.feels_like
   )}°C`;
+  temperatureRange.innerHTML = `${Math.round(
+    response.data.main.temp_min
+  )}°C / ${Math.round(response.data.main.temp_max)}°C`;
 }
 let apiKey = "653b806439282a7bd7eba7fc4fe797a3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=auckland&units=metric&appid=${apiKey}`;
