358933317abc49024dbb89a1991eb187fee3241d
AmandaJ17 <amanda.stacey17@gmail.com>
2020-10-28 16:32:34 +1300

    added city and description element in JS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-let apiKey = "653b806439282a7bd7eba7fc4fe797a3";
-let cityName = "Auckland";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityName = document.querySelector("#city");
+  let description = document.querySelector("#weatherDescription");
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+  cityName.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
 }
+let apiKey = "653b806439282a7bd7eba7fc4fe797a3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=auckland&units=metric&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
