ea0a99543316be6b440caf3b1d97abde4752c0f7
AmandaJ17 <amanda.stacey17@gmail.com>
2020-10-28 16:24:10 +1300

    Added current temp function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-function displayTemperature(response) {
-  console.log(response.data);
-}
-
 let apiKey = "653b806439282a7bd7eba7fc4fe797a3";
 let cityName = "Auckland";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+}
 
 axios.get(apiUrl).then(displayTemperature);
